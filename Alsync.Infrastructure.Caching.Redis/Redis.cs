44368fab6345e1ece3181645ac762a65ecb56cc6
Linkjr <linkjr@sina.com>
2020-04-02 03:22:11 +0800

    Add redis cache.

---
+++ Alsync.Infrastructure.Caching.Redis/Redis.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Redis;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alsync.Infrastructure.Caching.Redis
+{
+    public class Redis : ICache
+    {
+        private readonly RedisCache redisCache;
+
+        public Redis(IOptions<RedisCacheOptions> options)
+        {
+            this.redisCache = new RedisCache(options.Value);
+        }
+
+        public string Get(string key)
+        {
+            var bytes = this.redisCache.Get(key);
+            if (bytes == null || bytes.Length == 0)
+                return null;
+
+            var value = Encoding.UTF8.GetString(bytes);
+            return value;
+        }
+
+        public async Task<string> GetAsync(string key)
+        {
+            var bytes = await this.redisCache.GetAsync(key);
+            if (bytes == null || bytes.Length == 0)
+                return null;
+
+            var value = Encoding.UTF8.GetString(bytes);
+            return value;
+        }
+
+        public void Remove(string key)
+        {
+            this.redisCache.Remove(key);
+        }
+
+        public void Set(string key, string value, TimeSpan? expiry = null)
+        {
+            var expire = new DistributedCacheEntryOptions { AbsoluteExpirationRelativeToNow = expiry };
+            this.redisCache.Set(key, Encoding.UTF8.GetBytes(value), expire);
+        }
+
+        public async Task SetAsync(string key, string value, TimeSpan? expiry = null)
+        {
+            var expire = new DistributedCacheEntryOptions { AbsoluteExpirationRelativeToNow = expiry };
+            await this.redisCache.SetAsync(key, Encoding.UTF8.GetBytes(value), expire);
+        }
+    }
+}
---
+++ Alsync.Infrastructure.Caching.Redis/RedisCacheExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Caching.Redis;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Alsync.Infrastructure.Caching.Redis
+{
+    public static class RedisCacheExtensions
+    {
+        public static CacheBuilder AddRedisCache(this CacheBuilder builder, Action<RedisCacheOptions> setupAction)
+        {
+            if (builder == null)
+                throw new ArgumentNullException(nameof(builder));
+
+            if (setupAction == null)
+                throw new ArgumentNullException(nameof(setupAction));
+
+            var services = builder.Services;
+            services.Configure(setupAction);
+            services.AddSingleton<ICache, Redis>();
+
+            //var configuration = services.BuildServiceProvider()
+            //    .GetService<IConfiguration>();
+            //services.AddDistributedRedisCache(options =>
+            //{
+            //    options.Configuration = configuration["Redis:Configuration"];
+            //    options.InstanceName = configuration["Redis:InstanceName"];
+            //});
+
+            return builder;
+        }
+    }
+}
