52a746e105d137ac5054efa44a501e242f12b926
Linkjr <linkjr@sina.com>
2020-04-02 17:31:25 +0800

    Add servicelocator.

---
+++ Alsync.Infrastructure.DependencyInjection/ServiceLocator.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Alsync.Infrastructure.DependencyInjection
+{
+    public sealed class ServiceLocator
+    {
+        public static ServiceLocator Instance { get; } = new ServiceLocator();
+
+        private IServiceProvider serviceProvider;
+        public void ConfirgureServiceProvider(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+
+        public T GetService<T>()
+        {
+            return this.serviceProvider.GetRequiredService<T>();
+        }
+    }
+}
