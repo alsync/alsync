b19a307850a0ff4dfacd4d11404703d80b14ba4d
Linkjr <linkjr@sina.com>
2020-04-02 18:13:40 +0800

    Add memory cache project.

---
+++ Alsync.Infrastructure.Caching.Memory/Memory.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+using System;
+using System.Threading.Tasks;
+
+namespace Alsync.Infrastructure.Caching.Memory
+{
+    public class Memory : ICache
+    {
+        private readonly IMemoryCache memoryCache;
+
+        public Memory(IOptions<MemoryDistributedCacheOptions> options)
+        {
+            this.memoryCache = new MemoryCache(options);
+        }
+
+        public string Get(string key)
+        {
+            return this.memoryCache.Get<string>(key);
+        }
+
+        public Task<string> GetAsync(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Set(string key, string value, TimeSpan? expiry = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetAsync(string key, string value, TimeSpan? expiry = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Alsync.Infrastructure.Caching.Memory/MemoryExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Alsync.Infrastructure.Caching;
+using Alsync.Infrastructure.Caching.Memory;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class MemoryExtensions
+    {
+        public static CacheBuilder AddMemory(this CacheBuilder builder, Action<MemoryDistributedCacheOptions> setupAction)
+        {
+            if (builder == null)
+                throw new ArgumentNullException(nameof(builder));
+
+            if (setupAction == null)
+                throw new ArgumentNullException(nameof(setupAction));
+
+            var services = builder.Services;
+            services.Configure(setupAction);
+            services.AddSingleton<ICache, Memory>();
+
+            return builder;
+        }
+    }
+}
