4b0a55668698f77962167a73db0093f667013bd6
Linkjr <linkjr@sina.com>
2020-08-30 14:17:08 +0800

    Add heath check api.

---
+++ Alsync.Api/Controllers/HealthController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Alsync.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        /// <summary>
+        /// 健康检测心跳包地址。
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
