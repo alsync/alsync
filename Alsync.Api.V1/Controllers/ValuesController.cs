d12239b31c2919bf4eb3707585d3a28016ee69f5
Linkjr <linkjr@sina.com>
2020-09-02 19:58:49 +0800

    test.

--- Alsync.Api.V1/Controllers/ValuesController.cs
+++ Alsync.Api.V1/Controllers/ValuesController.cs
@@ -5,11 +5,19 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace Alsync.Api.V1.Controllers
 {
     public class ValuesController : HttpController
     {
+        private readonly IConfiguration _configuration;
+
+        public ValuesController(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
         /// <summary>
         /// Test get.
         /// </summary>
@@ -26,7 +34,7 @@ namespace Alsync.Api.V1.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "value";
+            return "value; Host:" + Request.Host + "; ports:" + this._configuration["port"];
         }
     }
 }
