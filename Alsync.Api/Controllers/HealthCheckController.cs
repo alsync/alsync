7018731887bceb1bce75217077f7a479028626de
Linkjr <linkjr@sina.com>
2020-09-02 19:09:17 +0800

    Rename HealthController as HealthCheckController.

--- Alsync.Api/Controllers/HealthCheckController.cs
+++ Alsync.Api/Controllers/HealthCheckController.cs
@@ -7,9 +7,12 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Alsync.Api.Controllers
 {
+    /// <summary>
+    /// 用于提供健康检测的控制器。
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
-    public class HealthController : ControllerBase
+    public class HealthCheckController : ControllerBase
     {
         /// <summary>
         /// 健康检测心跳包地址。
