b2e9076f6aa43676c3e1cc7d59d000e705a84000
Linkjr <linkjr@sina.com>
2020-09-15 10:53:04 +0800

    Add ocelot cache.

--- Alsync.Gateway/Startup.cs
+++ Alsync.Gateway/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Ocelot.Cache;
+using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Ocelot.Provider.Consul;
@@ -23,7 +25,14 @@ namespace Alsync.Gateway
         {
             services.AddOcelot()
                 .AddConsul()
+                //.AddCacheManager(setting =>
+                //{
+                //    setting.WithDictionaryHandle(); //默认是字典存储，可自定义为Redis等存储
+                //})
                 .AddPolly();
+
+            //// IOcelotCache<CachedResponse>是默认缓存约束，可替换成自定义的
+            //services.AddSingleton<IOcelotCache<CachedResponse>, CustomCache>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
